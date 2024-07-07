222be3c1bc6bad14b353331009133e9e9081248a
Agnideep Ghosh <95609274+ag-wnl@users.noreply.github.com>
2024-07-07 12:04:37 +0530

    chore: allow 3rd party transactions and make contract non authorative for Purps Coin

--- contracts/purpsToken.sol
+++ contracts/purpsToken.sol
@@ -1,19 +1,50 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract purps {
+contract purpsTokenContract {
+    string public name = "Purps";
+    string public symbol = "PURP";
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 1000000 * (10 ** uint256(decimals));
+
     address Deployer;
-    mapping(address => uint) balances; // address, balance key value pairs in a map
+    mapping(address => uint256) public balances; 
+    mapping(address => mapping(address => uint256)) public allowance; // enable 3rd party transacions
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
     constructor() {
         Deployer = msg.sender;
+        balances[Deployer] = totalSupply;
     }
 
-    function sendTokens(uint Amount, address Receiver) public{
-        require(msg.sender == Deployer, "Only deployer of the contract can send tokens."); 
-        balances[Receiver] += Amount;
+    function transferTokens(address receiver, uint256 amount) public returns (bool success) {
+        require(balances[msg.sender] >= amount, "Not enough balance.");
+
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Transfer(msg.sender, receiver, amount);
+        return true;
     }
 
     function viewBalance() public view returns (uint) {
         return balances[msg.sender];
+    }
+
+    function approveTransaction(address spender, uint256 amount) public returns (bool success) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferToAndFrom(address _from, address _to, uint256 amount) public returns (bool success) {
+        require(balances[_from] >= amount, "Not enough balance.");
+        require(allowance[_from][msg.sender] >= amount, "Allowance exceeded, not permitted transaction.");
+
+        balances[_from] -= amount;
+        balances[_to] += amount;
+        emit Transfer(_from, _to, amount);
+        return true;
     }
 }
