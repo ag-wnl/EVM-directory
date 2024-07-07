7f59afb30f00082c3ff2d5233845d6a2ffc6c074
Agnideep Ghosh <95609274+ag-wnl@users.noreply.github.com>
2024-07-07 12:36:38 +0530

    chore: erc-20 standard and burning logic

--- contracts/purpsToken.sol
+++ contracts/purpsToken.sol
@@ -8,7 +8,7 @@ contract purpsTokenContract {
     uint256 public totalSupply = 1000000 * (10 ** uint256(decimals));
 
     address Deployer;
-    mapping(address => uint256) public balances; 
+    mapping(address => uint256) public balanceOf; 
     mapping(address => mapping(address => uint256)) public allowance; // enable 3rd party transacions
 
     event Transfer(address indexed from, address indexed to, uint256 value);
@@ -16,35 +16,41 @@ contract purpsTokenContract {
 
     constructor() {
         Deployer = msg.sender;
-        balances[Deployer] = totalSupply;
+        balanceOf[Deployer] = totalSupply;
+        emit Transfer(address(0), Deployer, totalSupply);
     }
 
-    function transferTokens(address receiver, uint256 amount) public returns (bool success) {
-        require(balances[msg.sender] >= amount, "Not enough balance.");
-
-        balances[msg.sender] -= amount;
-        balances[receiver] += amount;
+    function transfer(address receiver, uint256 amount) public returns (bool success) {
+        require(balanceOf[msg.sender] >= amount, "Not enough balance.");
+        require(receiver != address(0), "Invalid address.");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[receiver] += amount;
         emit Transfer(msg.sender, receiver, amount);
         return true;
     }
 
-    function viewBalance() public view returns (uint) {
-        return balances[msg.sender];
-    }
-
-    function approveTransaction(address spender, uint256 amount) public returns (bool success) {
+    function approve(address spender, uint256 amount) public returns (bool success) {
         allowance[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
     }
 
-    function transferToAndFrom(address _from, address _to, uint256 amount) public returns (bool success) {
-        require(balances[_from] >= amount, "Not enough balance.");
+    function transferFrom(address _from, address _to, uint256 amount) public returns (bool success) {
+        require(balanceOf[_from] >= amount, "Not enough balance.");
         require(allowance[_from][msg.sender] >= amount, "Allowance exceeded, not permitted transaction.");
 
-        balances[_from] -= amount;
-        balances[_to] += amount;
+        balanceOf[_from] -= amount;
+        balanceOf[_to] += amount;
         emit Transfer(_from, _to, amount);
+        return true;
+    }
+
+    function _burn(address _from, uint256 amount) public returns (bool success) {
+        require(balanceOf[_from] >= amount, "Not enough balance");
+        
+        balanceOf[_from] -= amount;
+        totalSupply -= amount;
+        emit Transfer(_from, address(0), amount);
         return true;
     }
 }
